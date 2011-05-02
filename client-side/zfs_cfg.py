386c07043dc3e238fdf439f02e27fbabdced33f5
Cédric BRINER <Cedric.Briner@UniGE.ch>
2011-05-02 11:32:11 +0200

    remove old zfs files

--- client-side/zfs_cfg.py
+++
@@ -1,8 +0,0 @@
-# default configuration (<yellow_value>, <red_value>)
-CAPACITY_TRESHOLD=(80,90)
-
-# configuration tuning given in a list
-# it supports only regex for the first field
-LCAPACITY_TRESHOLD_TUNING=[
-# ('root_pool', 5, 10)
-]
